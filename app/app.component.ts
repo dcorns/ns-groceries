009d3de53e0e17989c1b37d376b91f39654b71b3
Dale <drcorns@hotmail.com>
2017-03-19 19:18:11 -0700

    using ternary operator in templates to toggle

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,15 +8,19 @@ import {Component} from "@angular/core";
     <TextField hint="Email Address" keyboardType="email" [(ngModel)] = "email"
 autocorrect="false" autocapitalizationType="none"></TextField>
     <TextField hint="Password" secure="true"></TextField>
-    <Button text="Sign in" class="submit-button" (tap)="submit()"></Button>
-    <Button text="Sign up for Groceries"></Button>
+    <Button [text]="isLoggingIn ? 'Sign in' : 'Sign up'" class="submit-button" (tap)="submit()"></Button>
+    <Button [text]="isLoggingIn ? 'Sign up' : 'Back to login'" (tap)="toggleDisplay()"></Button>
 </StackLayout>
 `,
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 export class AppComponent {
   email = "codefellow@gmail.com";
+  isLoggingIn = true;
   submit(){
     alert("You're using: " + this.email);
+  }
+  toggleDisplay(){
+    this.isLoggingIn = !this.isLoggingIn;
   }
 }
