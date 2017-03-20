18d33689f7cba2e6d197994f318e9b64fbc6c619
Dale <drcorns@hotmail.com>
2017-03-20 15:18:58 -0700

    registration is working

---
+++ platforms/android/src/main/assets/app/pages/login/login.component.ts
@@ -0,0 +1,42 @@
+import {Component} from "@angular/core";
+import {User} from "../../shared/user/user";
+import {UserService} from "../../shared/user/user.service"
+@Component({
+  selector: "my-app",
+  providers: [UserService],
+  templateUrl: "pages/login/login.html",
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+})
+export class LoginComponent {
+  user: User;
+  isLoggingIn = true;
+  constructor(private userService: UserService){
+    this.user = new User();
+    this.user.email = "user@dale.com";
+    this.user.password = "123erd";
+  }
+  submit(){
+    if(this.isLoggingIn){
+      this.login();
+    }else{
+      this.signUp();
+    }
+  }
+  login(){
+
+  }
+  signUp(){
+    this.userService.register(this.user)
+    //subscribing to the http.post observer
+      .subscribe(
+        () => {
+          alert("Your account was successfully created.");
+          this.toggleDisplay();
+        },
+        () => alert("Unfortunately we were unable to create your account.")
+      );
+  }
+  toggleDisplay(){
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
