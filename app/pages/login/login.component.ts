93cbb7154f8c8efb3b43b86318a559201435d419
Dale <drcorns@hotmail.com>
2017-03-20 17:08:44 -0700

    simple animation with the View class inherited by all NativeScript UI elements

--- app/pages/login/login.component.ts
+++ app/pages/login/login.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, ViewChild, ElementRef} from "@angular/core";
 import {User} from "../../shared/user/user";
 import {UserService} from "../../shared/user/user.service";
 import {Router} from "@angular/router";
 import {Page} from "ui/page";
+import {Color} from 'color';
+import {View} from 'ui/core/view';
 @Component({
   selector: "my-app",
   providers: [UserService],
@@ -12,6 +14,7 @@ import {Page} from "ui/page";
 export class LoginComponent implements OnInit{
   user: User;
   isLoggingIn = true;
+  @ViewChild('container') container: ElementRef;
   constructor(private router: Router, private userService: UserService, private page: Page){
     this.user = new User();
     this.user.email = "user@dale.com";
@@ -48,5 +51,10 @@ export class LoginComponent implements OnInit{
   }
   toggleDisplay(){
     this.isLoggingIn = !this.isLoggingIn;
+    let container = <View>this.container.nativeElement;
+    container.animate({
+      backgroundColor: this.isLoggingIn ? new Color('white') : new Color('#301217'),
+      duration: 200
+    });
   }
 }
